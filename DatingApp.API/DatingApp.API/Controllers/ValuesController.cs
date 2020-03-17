42f101b8678c53a29bb6b65b1886f8cf313e4dbf
Ziham <zihambaba18@gmail.com>
2020-03-17 10:23:34 +0530

    Added : Security (bearer token)

--- DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/DatingApp.API/Controllers/ValuesController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -29,7 +31,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
-
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
@@ -43,6 +45,7 @@ namespace DatingApp.API.Controllers
         {
         }
 
+     
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
---
+++ DatingApp.API/DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dtos
+{
+
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        [StringLength(10,MinimumLength =6, ErrorMessage = "Too short")]
+        public string Password { get; set; }
+
+    }
+}
